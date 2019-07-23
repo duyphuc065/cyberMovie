67c83a77a8c6b1d23c54a19271203a0f8c1662ef
nguyendp <nguyendp@nct.vn>
2019-07-23 10:13:50 +0700

    update code slick

--- src/app/home/news/news.component.ts
+++ src/app/home/news/news.component.ts
@@ -1,15 +1,103 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+declare var $: any;
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+  selector: "app-news",
+  templateUrl: "./news.component.html",
+  styleUrls: ["./news.component.scss"]
 })
 export class NewsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    console.log(324);
+    var gadgetCarousel = $(".sliders");
+
+    gadgetCarousel.each(function() {
+      if ($(this).is(".slick-film")) {
+        $(this).slick({
+          prevArrow:
+            '<img class="left-arrow" src="assets/img/icon/back-session.png">',
+          nextArrow:
+            '<img class="right-arrow" src="assets/img/icon/next-session.png">',
+          infinite: true,
+          speed: 300,
+          slidesPerRow: 4,
+          rows: 2,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesPerRow: 3,
+                rows: 2,
+                infinite: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesPerRow: 2,
+                rows: 2,
+                infinite: true,
+                arrows: false,
+                dot: false
+              }
+            }
+          ]
+        });
+      } else if ($(this).is(".slick-soon")) {
+        $(this).slick({
+          prevArrow:
+            '<img class="left-arrow" src="assets/img/icon/back-session.png">',
+          nextArrow:
+            '<img class="right-arrow" src="assets/img/icon/next-session.png">',
+          infinite: true,
+          speed: 300,
+          slidesPerRow: 4,
+          rows: 2,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesPerRow: 3,
+                rows: 2,
+                infinite: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesPerRow: 2,
+                rows: 2,
+                infinite: true,
+                arrows: false,
+                dot: false
+              }
+            }
+          ]
+        });
+      } else if ($(this).is(".slick-news")) {
+        $(this).slick({
+          prevArrow:
+            '<img class="left-arrow" src="assets/img/icon/back-session.png">',
+          nextArrow:
+            '<img class="right-arrow" src="assets/img/icon/next-session.png">',
+          centerMode: true,
+          variableWidth: true,
+          focusOnSelect: true,
+          slidesToShow: 3,
+          responsive: [
+            {
+              breakpoint: 600,
+              arrows: false
+            }
+          ]
+        });
+      } else {
+        $(this).slick();
+      }
+    });
   }
-
 }
